2cd554bfe16295c410f12eac431be414bc0e36e9
hashem <hashemalsaie0457@gmail.com>
2024-12-03 19:34:28 +0000

    asd

--- index.js
+++ index.js
@@ -1,5 +1,55 @@
-import { LEVEL, OBJECT_TYPE } from "./setup";
+import { LEVEL, OBJECT_TYPE} from "./setup.js";
 
-
+import { GameBoard } from './GameBoard.js';
+import { Pacman } from "./Pacman.js";
+ 
 const gameGrid = document.querySelector('#game');
+const scoreTable = document.querySelector('#score');
+const startButton = document.querySelector('#start-button');
 
+const POWER_PILL_TIME = 10000;
+const GLOBAL_SPEED = 80;
+const gameBoard = GameBoard.createGameBoard(gameGrid, LEVEL);
+
+let score = 0;
+let timer = null;
+let gameWin = false;
+let powerPillActive = false;
+let powerPillTimer = null;
+
+
+function gameOver(pacman, grid){
+
+
+}
+
+function checkCollision(pacman, ghosts) {
+
+}
+
+function gameLoop(pacman, ghosts){
+
+
+}
+
+function startGame(){
+    gameWin = false;
+    powerPillActive = false;
+    score = 0;
+
+    startButton.classList.add('hide'); 
+
+
+    gameBoard.createGrid(LEVEL);
+
+    const pacman = new Pacman(2,287);
+
+    gameBoard.addObject(287, [OBJECT_TYPE.PACMAN]);
+    document.addEventListener('keydown', (e) =>
+    pacman.handleKeyInput(e, gameBoard.objectExist.bind(gameBoard))
+    );
+
+}
+
+
+startButton.addEventListener('click', startGame);
